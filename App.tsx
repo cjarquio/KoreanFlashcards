3254524d6576de6667e1317b23deb3afb5e17e0b
Charles Jarquio <cjarquio@gmail.com>
2025-05-20 19:04:53 -0700

    Change api authorize to use apiKey

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, View, StyleSheet } from 'react-native';
+import { Button, View, StyleSheet, SafeAreaView } from 'react-native';
 import { Amplify } from 'aws-amplify';
 import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
 import outputs from './amplify_outputs.json';
@@ -7,25 +7,10 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "./amplify/data/resource";
 
 Amplify.configure(outputs);
-const client = generateClient<Schema>()
 
 const SignOutButton = () => {
-  const [things, setThings] = useState<any>();
   const { signOut } = useAuthenticator();
-  useEffect (() => {
-    const fetchData = async () => {
-      try {
-        const { data: translations } = await client.models.koreanenglish.list()
-        setThings(translations)
-      }
-      catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
-
-    fetchData();
-  },[])
-  console.log(things)
+  
   return (
     <View style={styles.signOutButton}>
       <Button title="Sign Out" onPress={signOut} />
@@ -36,21 +21,45 @@ const SignOutButton = () => {
 // Amplify 6 has Authenticator that incompatible with current version of Expo Go.
 // Would need to run app in Android Emulator or iOS Simulator.
 const App = () => {
-  
+  const [things, setThings] = useState<any>();
+  const client = generateClient<Schema>()
+
+  useEffect (() => {
+    const fetchData = async () => {
+      try {
+        const { data: translations, errors } = await client.models.koreanenglish.list()
+        if(errors) {
+          console.error('Error fetching data:', errors);
+        }
+        setThings(translations)
+      }
+      catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  },[])
+  console.log(things)
 
   return (
     <Authenticator.Provider>
       <Authenticator>
-        
-        <SignOutButton />
+        <SafeAreaView style={styles.container}>
+          <SignOutButton />
+        </SafeAreaView>
       </Authenticator>
     </Authenticator.Provider>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 8,
+  },
   signOutButton: {
-    alignSelf: 'flex-end',
+    alignSelf: "flex-end",
   },
 });
 
